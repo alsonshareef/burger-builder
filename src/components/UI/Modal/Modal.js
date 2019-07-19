0eb7dabe90f04a5db611554310e7f443a321fbbe
Alson Shareef <alson.shareef@gmail.com>
2019-07-19 18:28:32 +0800

    Imported and placed Backdrop component next to main Modal div, and wrapped both JSX elements with default Aux HOC so they can be adjacent to each other.

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,19 +1,28 @@
 import React from "react";
 import classes from "./Modal.css";
 
+import Aux from "../../../hoc/Auxiliary";
+import Backdrop from "../Backdrop/Backdrop";
+
 const modal = props => {
 	return (
-		<div
-			className={classes.Modal}
-			style={{
-				transform: props.purchasing
-					? "translateY(0)"
-					: "translateY(-100vh)",
-				opacity: props.purchasing ? "1" : "0"
-			}}
-		>
-			{props.children}
-		</div>
+		<Aux>
+			<Backdrop
+				purchasing={props.purchasing}
+				cancel={props.cancelBackdrop}
+			/>
+			<div
+				className={classes.Modal}
+				style={{
+					transform: props.purchasing
+						? "translateY(0)"
+						: "translateY(-100vh)",
+					opacity: props.purchasing ? "1" : "0"
+				}}
+			>
+				{props.children}
+			</div>
+		</Aux>
 	);
 };
 

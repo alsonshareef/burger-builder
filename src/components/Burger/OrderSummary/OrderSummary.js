368009ebf84b0634635efe26af00a4372b3c1fb5
Alson Shareef <alson.shareef@gmail.com>
2019-07-19 16:25:57 +0800

    Dynamically lists out the amount of every ingredient in the users burger before checkout.

---
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Aux from "../../../hoc/Auxiliary";
+
+const orderSummary = props => {
+	const ingredientSummary = Object.keys(props.ingredients).map(key => {
+		return (
+			<li key={key}>
+				<span style={{ textTransform: "capitalize" }}>{key}</span>:{" "}
+				{props.ingredients[key]}
+			</li>
+		);
+	});
+	return (
+		<Aux>
+			<h3>Your Order</h3>
+			<p>Your burger's ingredients:</p>
+			<ul>{ingredientSummary}</ul>
+		</Aux>
+	);
+};
+
+export default orderSummary;

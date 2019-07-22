68807c57e6d002ce38c1d07fd26eddb8407c953c
Alson Shareef <alson.shareef@gmail.com>
2019-07-22 20:39:50 +0800

    Double quotes turned to single quotes.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React, { Component } from "react";
-import "./App.css";
+import React, { Component } from 'react';
+import './App.css';
 
-import Layout from "./components/Layout/Layout";
-import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
+import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 export default class App extends Component {
 	render() {

65f538cead8c12c2c25bc2abd1a21ca445b4161f
Alson Shareef <alson.shareef@gmail.com>
2019-07-24 19:45:07 +0800

    Removed props

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -4,7 +4,7 @@ import classes from './Toolbar.css';
 import Logo from '../../Logo/Logo';
 import NavItems from '../NavItems/NavItems';
 
-const toolbar = props => (
+const toolbar = () => (
 	<header className={classes.Toolbar}>
 		<div>MENU</div>
 		<Logo />

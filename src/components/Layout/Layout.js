7fb58855d71690b52346feba99269afd77d0397f
Alson Shareef <alson.shareef@gmail.com>
2019-07-24 19:46:11 +0800

    Added Sidebar component to be displayed when the viewport width is lower than 500px

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,11 +3,13 @@ import classes from './Layout.css';
 
 import Aux from '../../hoc/Auxiliary';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import Sidebar from '../Navigation/Sidebar/Sidebar';
 
 const layout = props => {
 	return (
 		<Aux>
 			<Toolbar />
+			<Sidebar />
 			<main className={classes.Content}>{props.children}</main>
 		</Aux>
 	);

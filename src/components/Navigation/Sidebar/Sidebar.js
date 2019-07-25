6d6bf5a057ed759bb21d2c971d966d2c3823c966
Alson Shareef <alson.shareef@gmail.com>
2019-07-25 16:33:48 +0800

    Placed Backdrop adjacent to original Sidebar div, and wrapped both with Aux HOC so it could work. Also created a conditional to set open and close transitions through classes for the Sidebar.

--- src/components/Navigation/Sidebar/Sidebar.js
+++ src/components/Navigation/Sidebar/Sidebar.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import classes from './Sidebar.css';
 
+import Aux from '../../../hoc/Auxiliary';
 import Logo from '../../Logo/Logo';
 import NavItems from '../NavItems/NavItems';
+import Backdrop from '../../UI/Backdrop/Backdrop';
 
-const sidebar = () => {
+const sidebar = props => {
+	let attachedClasses = [classes.Sidebar, classes.Close];
+	if (props.open) {
+		attachedClasses = [classes.Sidebar, classes.Open];
+	}
+
 	return (
-		<div className={classes.Sidebar}>
-			<Logo height="11%" marginBottom="32px" />
-			<nav>
-				<NavItems />
-			</nav>
-		</div>
+		<Aux>
+			<Backdrop show={props.open} click={props.close} />
+			<div className={attachedClasses.join(' ')}>
+				<Logo height="11%" marginBottom="32px" />
+				<nav>
+					<NavItems />
+				</nav>
+			</div>
+		</Aux>
 	);
 };
 

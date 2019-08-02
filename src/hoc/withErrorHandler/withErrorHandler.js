0d31d056230fd7fef888720cc75b642c366dffa6
Alson Shareef <alson.shareef@gmail.com>
2019-08-02 14:50:17 +0800

    Moved axios interceptor initialization into the custructor so that it initializes before withErrorHandler HOC mounts, so that the Modal with its error message will be rendered if there are any errors occuring with child components using axios HTTP library.

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -5,11 +5,11 @@ import Modal from '../../components/UI/Modal/Modal';
 
 const withErrorHandler = (WrappedComponent, axios) => {
 	return class extends Component {
-		state = {
-			error: null
-		};
-
-		componentDidMount() {
+		constructor() {
+			super();
+			this.state = {
+				error: null
+			};
 			axios.interceptors.request.use(req => {
 				this.setState({ error: null });
 				return req;
